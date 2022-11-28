a062c398835022e2316d2436094e451e9c05be4f
Shafira Khr <76768695+yokasxs@users.noreply.github.com>
2022-11-28 18:00:36 +0700

    change command exec

--- app/app.py
+++ app/app.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 	# Load model yang telah ditraining
 	# model = load('model_iris_dt.model')
 
-	exec('sudo fuser -k 4829/tcp')
+	exec('fuser -k 4829/tcp')
 	# Run Flask di localhost 
 	app.run(host="localhost", port=4829, debug=False)
 	
