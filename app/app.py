fd4779f499ca68b297784dfdef2471998e2d4e62
Shafira Khr <76768695+yokasxs@users.noreply.github.com>
2022-11-28 17:51:25 +0700

    change port 4321

--- app/app.py
+++ app/app.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
 	# model = load('model_iris_dt.model')
 
 	# Run Flask di localhost 
-	app.run(host="localhost", port=8080, debug=False)
+	app.run(host="localhost", port=4321, debug=False)
 	
 	
 
